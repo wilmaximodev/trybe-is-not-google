6abb946cc9d11432a7af2140abc7064a6e86c68f
Wilson Maximo <wilson-max@hotmail.com>
2024-01-02 15:37:53 -0300

    Req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -7,7 +7,8 @@ def exists_word(word, instance):
         occurrences = []
 
         # Itera sobre cada linha do arquivo
-        for line_number, line in enumerate(file_data["linhas_do_arquivo"], start=1):
+        for line_number, line in enumerate(
+                file_data["linhas_do_arquivo"], start=1):
             if word.lower() in line.lower():
                 occurrences.append({"linha": line_number})
 
@@ -22,4 +23,27 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_list = []
+
+    # Itera sobre cada arquivo na fila
+    for file_data in instance.queue:
+        file_name = file_data["nome_do_arquivo"]
+        occurrences = []
+
+        # Itera sobre cada linha do arquivo
+        for line_number, line_content in enumerate(
+                file_data["linhas_do_arquivo"], start=1):
+            # Realiza a busca case-insensitive na linha
+            if word.lower() in line_content.lower():
+                # Adiciona informações da ocorrência à lista
+                occurrences.append(
+                    {"linha": line_number, "conteudo": line_content})
+
+        if occurrences:
+            result_list.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return result_list
