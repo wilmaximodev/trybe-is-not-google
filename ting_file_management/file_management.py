718d5ad61538386b370af397fe8ed085d0d04c7b
Wilson Maximo <wilson-max@hotmail.com>
2024-01-02 14:54:47 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Verifica se o arquivo possui a extensão .txt
+    if not path_file.endswith('.txt'):
+        print("Formato inválido. O arquivo deve ter extensão .txt",
+              file=sys.stderr)
+        return []
+
+    try:
+        # Tenta abrir o arquivo em modo de leitura ('r')
+        with open(path_file, 'r') as file:
+            # Lê todas as linhas do arquivo e as armazena em uma lista
+            lines = file.read().split('\n')
+            return lines
+    except FileNotFoundError:
+        # Se o arquivo não for encontrado, imprime uma mensagem de erro
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
